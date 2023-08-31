a7f176f2990d1b9dcdfa22406bbb2b9104524d30
신형기 <tkaqkeldk@hanatour.com>
2023-08-31 19:06:46 +0900

    refactor: 어플리케이션 컨텍스트를 띄우고 빈을 등록하는 방향으로 수정

--- src/main/java/com/example/hntspringpractice/HntSpringPracticeApplication.java
+++ src/main/java/com/example/hntspringpractice/HntSpringPracticeApplication.java
@@ -1,25 +1,25 @@
 package com.example.hntspringpractice;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
-//@SpringBootApplication
 public class HntSpringPracticeApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(HntSpringPracticeApplication.class, args);
+        // ApplicationContext 생성 및 초기화.
+        GenericApplicationContext context = new GenericApplicationContext();
 
-        // ApplicationContext 를 생성하고 초기화 한다.
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // helloWorldService 라는 이름으로 HelloWorldService 클래스를 빈으로 등록한다.
+        context.registerBean("helloWorldService", HelloWorldService.class, HelloWorldService::new);
 
-        // 컨텍스트에서 Bean 을 가져온다.
-        HelloWorldService helloWorldService = (HelloWorldService) context.getBean("helloWorldService");
+        // 빈 등록을 완료하기 위해 ApplicationContext 를 갱신한다.
+        //  Spring 컨텍스트는 빈의 등록과 초기화를 완료하고, 애플리케이션에서 이를 올바르게 사용할 준비를 마치게 된다고 한다.
+        context.refresh();
+
+        // 컨텍스트에서 등록했던 빈을 가져온다.
+        HelloWorldService helloWorldService = context.getBean("helloWorldService", HelloWorldService.class);
 
         // sayHello() 메서드를 호출하여 "Hello World"를 출력한다.
         helloWorldService.sayHello();
-
-        // 사용이 끝난 컨텍스트를 닫는다.
-        context.close();
     }
 
 }
