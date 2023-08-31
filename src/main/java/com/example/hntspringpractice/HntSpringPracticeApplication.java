4267638ee548d103b4902f8cdb79517359ddd935
신형기 <tkaqkeldk@hanatour.com>
2023-08-31 18:33:01 +0900

    chore: initial Commit

---
+++ src/main/java/com/example/hntspringpractice/HntSpringPracticeApplication.java
@@ -0,0 +1,15 @@
+package com.example.hntspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class HntSpringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HntSpringPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hntspringpractice/HntSpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hntspringpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HntSpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
